c21bf1bf4d05827a7e5389caa07f3803b5acfdcb
AnotonioCeva3150 <hugo.cevallos3150@utc.edu.ec>
2026-06-04 15:44:43 -0500

    taty1

--- app/src/main/java/com/example/TEST/App.java
+++ app/src/main/java/com/example/TEST/App.java
@@ -11,7 +11,7 @@ public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Ingresa la longitud de la circunferencia: ");
+        System.out.print("Ingresa un numeroe la circunferencia: ");
         double C = sc.nextDouble();
 
         double radio = C / (2 * Math.PI);

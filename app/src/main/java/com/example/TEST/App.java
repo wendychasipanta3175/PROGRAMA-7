3b128f1e8bc665fa2c4f725c9b435b401a0a4aa5
wendy chasipanta <wendy.chasipanta3175@utc.edu.ec>
2026-06-04 13:39:34 -0500

    test de prueba

---
+++ app/src/main/java/com/example/TEST/App.java
@@ -0,0 +1,15 @@
+package com.example.TEST;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Ingresa la longitud de la circunferencia: ");
+        double C = sc.nextDouble();
+
+        double radio = C / (2 * Math.PI);
+
+        System.out.println("El radio es: " + radio);
+    }
+}

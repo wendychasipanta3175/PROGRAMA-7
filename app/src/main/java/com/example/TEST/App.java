8a2a9f5f0913aeb1433248dc402a14ac60b3d749
DiegoFlores20 <floresdiego2006a@gmail.com>
2026-06-04 13:55:08 -0500

    Agregada la clase de login

--- app/src/main/java/com/example/TEST/App.java
+++ app/src/main/java/com/example/TEST/App.java
@@ -10,6 +10,6 @@ public class App {
 
         double radio = C / (2 * Math.PI);
 
-        System.out.println("El radio es: " + radio);
+        System.out.println("El radio de tal tal de tal cosa es: " + radio);
     }
 }

4ece7211ed3e85ea4fe19439d9e049c4ed168d1d
AnotonioCeva3150 <hugo.cevallos3150@utc.edu.ec>
2026-06-04 15:06:48 -0500

    noemi 2

--- app/src/main/java/com/example/TEST/App.java
+++ app/src/main/java/com/example/TEST/App.java
@@ -1,5 +1,7 @@
 package com.example.TEST;
 import java.util.Scanner;
+//hola
+//HOLA TATY
 
 public class App {
     public static void main(String[] args) {
@@ -10,6 +12,6 @@ public class App {
 
         double radio = C / (2 * Math.PI);
 
-        System.out.println("El radio de  de tal cosa es: " + radio);
+        System.out.println("El radio de tal tal de tal cosa es: " + radio);
     }
 }

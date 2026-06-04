2ebd18d84876ff8634326ad24439410f48418619
wendy chasipanta <wendy.chasipanta3175@utc.edu.ec>
2026-06-04 15:19:38 -0500

    wendy2

--- app/src/main/java/com/example/TEST/App.java
+++ app/src/main/java/com/example/TEST/App.java
@@ -15,6 +15,6 @@ public class App {
 
         double radio = C / (2 * Math.PI);
 
-        System.out.println("El radio de tal tal de tal cosa es: " + radio);
+        System.out.println("El radio de la circunferencia es: " + radio);
     }
 }
